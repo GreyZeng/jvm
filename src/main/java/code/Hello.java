e976f956acc5bfe9840ab1da2b5b12d58caf951f
GreyZeng <410486047@qq.com>
2021-10-18 19:02:04 +0800

    update notes

---
+++ src/main/java/code/Hello.java
@@ -0,0 +1,10 @@
+package code;
+
+public class Hello {
+    public static void main(String[] args) {
+        new Hello().add(2,3);
+    }
+    public void add(int a, int b) {
+        int c = a + b;
+    }
+}
