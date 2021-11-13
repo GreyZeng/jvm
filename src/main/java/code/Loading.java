24e8e43b330766aed049b99b4bb713f975209bc3
GreyZeng <410486047@qq.com>
2021-11-14 00:56:25 +0800

    modfiy code

--- src/main/java/code/Loading.java
+++ src/main/java/code/Loading.java
@@ -8,8 +8,8 @@ public class Loading {
 
 class T {
     // 以下两行掉换位置结果不一样
-    public static T t = new T();
     public static int count = 2;
+    public static T t = new T();
 
     private T() {
         count++;
