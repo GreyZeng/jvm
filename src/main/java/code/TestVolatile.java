776d9517226cdfad329cc86b0474e2a8da8ab8a2
GreyZeng <410486047@qq.com>
2021-11-14 18:42:55 +0800

    update

--- src/main/java/code/TestVolatile.java
+++ src/main/java/code/TestVolatile.java
@@ -2,10 +2,11 @@ package code;
 
 // 观察volatile的字节码
 public class TestVolatile {
-    private  int i;
+    private int i;
     volatile int j;
 
     public static void main(String[] args) {
-
+        short[] arr = {1, 2};
+        System.out.println(arr.getClass());
     }
 }
