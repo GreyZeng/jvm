312bf5b9037fc718cbf0ca1c95448739bc684441
GreyZeng <410486047@qq.com>
2021-11-26 20:13:34 +0800

    update

---
+++ src/main/java/code/HeapOOM.java
@@ -0,0 +1,24 @@
+package code;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * VM args: -Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+ *
+ * @author <a href="mailto:410486047@qq.com">Grey</a>
+ * @date 2021/11/26
+ * @since
+ */
+public class HeapOOM {
+    static class OOMObject {
+
+    }
+
+    public static void main(String[] args) {
+        List<OOMObject> list = new ArrayList<>();
+        while (true) {
+            list.add(new OOMObject());
+        }
+    }
+}
